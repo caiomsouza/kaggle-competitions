94433ab8f6f80c14e982c9894298e76fbfb2c842
Caio Moreno de Souza <caiomsouza@gmail.com>
2016-06-20 22:45:40 +0200

    add

--- santander-customer-satisfaction/src/install_ml_packages.R
+++ santander-customer-satisfaction/src/install_ml_packages.R
@@ -2,7 +2,23 @@
 # Caio github.com/caiomsouza
 
 
-install.packages('caret', repos='http://cran.rstudio.com/', dependencies = TRUE)
+# https://scottishsnow.wordpress.com/software/
+
+# Install R in Ubuntu
+# sudo apt-get update
+# sudo apt-get install r-base
+
+# Install RStudio in Ubuntu
+# wget http://download1.rstudio.org/rstudio-0.98.1062-amd64.deb
+# sudo dpkg -i *.deb
+# rm *.deb
+
+# Run Install R Script
+# sudo Rscript install_ml_packages.R
+
+
+
+install.packages("caret", repos='http://cran.rstudio.com/', dependencies = TRUE)
 install.packages("sas7bdat", repos='http://cran.rstudio.com/', dependencies = TRUE)
 install.packages("caret", repos='http://cran.rstudio.com/', dependencies = TRUE)
 install.packages("gbm", repos='http://cran.rstudio.com/', dependencies = TRUE)
