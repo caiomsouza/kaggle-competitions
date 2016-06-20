4d5f4d2e9e1a6eae4b4c2d0bb5b3795974d74706
Caio Moreno de Souza <caiomsouza@gmail.com>
2016-06-20 23:31:56 +0200

    Last commit before sending to Javier Portela

--- santander-customer-satisfaction/src/setup/install_ggplot2.R
+++ santander-customer-satisfaction/src/setup/install_ggplot2.R
@@ -2,6 +2,12 @@
 # Caio github.com/caiomsouza
 
 
+# sudo apt-get install r-base r-base-dev
+# sudo apt-get upgrade r-base r-base-dev
+
+# R Spain
+# https://cran.rediris.es/src/contrib
+
 sessionInfo()
 
 install.packages("ggplot2", repos='http://cran.rstudio.com/', dependencies = TRUE)
